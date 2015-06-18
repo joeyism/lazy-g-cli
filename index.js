088fb69417c1dd67c1adfc3c6ff5e0c5525ee028
joeyism <sham.joey@gmail.com>
2015-06-17 21:09:23 -0400

    first working commit

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+'use strict';
+var find = require('./lib/find');
+var exec = require('./lib/exec');
+var params = process.argv;
+params.splice(0,2);
+
+find.type().then(function(command){
+    
+    return exec(command, params);
+
+}).catch(function(err){
+
+    console.log(err.toString().red);
+
+})
